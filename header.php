cf57b9c61aef63a7b528621324af135ff8f5eb72
lh535 <hofertlisa@gmail.com>
2020-05-03 21:20:43 +0200

    add basic header

---
+++ header.php
@@ -0,0 +1,22 @@
+<div id="header">
+  <div class="header-object" id="header-home">
+    <a class="header-link" href='#'>Ferienwohnung Kirschengarten</a>
+  </div>
+  
+  <div id="header-right-container">
+    <div class="header-object" id="header-wohnung">
+      <a class="header-link" href='#'>Wohnung</a>
+    </div>
+    <div class="header-object" id="header-kaiserstuhl">
+      <a class="header-link" href='#'>Kaiserstuhl</a>
+    </div>
+    <div class="header-object" id="header-buchung">
+      <a class="header-link" href='#'>Buchung</a>
+    </div>
+    <div class="header-object" id="header-anfahrt">
+      <a class="header-link" href='#'>Anfahrt</a>
+    </div>
+  </div>
+</div>
+  
+  
--- index.php
+++ index.php
@@ -5,8 +5,12 @@
     <title>Ferienwohnung Kirschengarten</title>
     <link rel='stylesheet' type='text/css' href='styles/normalize.css'>
     <link rel='stylesheet' type='text/css' href='styles/custom-reset.css'>
+    <link rel='stylesheet' type='text/css' href='styles/main.css'>
+
   </head>
   <body>
-
+    <?php include 'header.php'; ?>
+ 
+    <p>test</p>
   </body>
 </html>
