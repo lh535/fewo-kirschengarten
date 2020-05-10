7ae3a83fae92a4d1f0a23dd0e75cef0e7419f907
lh535 <hofertlisa@gmail.com>
2020-05-10 23:23:18 +0200

    make navbar prettier + improve javascript click functionality

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
   
   <div id="header-right-container">
     <div class="dropdown"> 
-      <button class="dropdown-btn header-main" href='#' type='button'>Wohnung</button>
+      <button class="dropdown-btn header-main" href='#' type='button'>Wohnung <span class='dropdown-triangle'>&#x25BC;</span></button>
       <div class="dropdown-content">
         <a href='#'>Beschreibung</a>
         <a href='#'>Ausstattung</a>
@@ -12,7 +12,7 @@
     </div>
 
     <div class="dropdown">
-      <button class="dropdown-btn header-main"  type='button'>Kaiserstuhl</button>
+      <button class="dropdown-btn header-main"  type='button'>Kaiserstuhl <span class='dropdown-triangle'>&#x25BC;</span></button>
       <div class="dropdown-content">
         <a href='#'>Königschaffhausen</a>
         <a href='#'>Umgebung</a>
@@ -20,7 +20,7 @@
     </div>
 
     <div class="dropdown">
-      <button class="dropdown-btn header-main"  type='button'>Buchung</button>
+      <button class="dropdown-btn header-main"  type='button'>Buchung <span class='dropdown-triangle'>&#x25BC;</span></button>
       <div class="dropdown-content">
         <a href='#'>Preisliste</a>
         <a href='#'>Belegungsplan</a>
--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,6 +1,67 @@
+// toggle drop down menu. argument is surrounding div for button.
+// by default just toggle, or provide additional arguments to only deactivate/activate. If both are true, do nothing
+function toggleDropdown(dropdownDiv, justActivate=false, justDeactivate=false) {
+  let dropdownMenu = dropdownDiv.querySelector(".dropdown-content");
+  if (!justActivate && !justDeactivate) {
+    dropdownMenu.classList.toggle("active");
+    dropdownDiv.classList.toggle("active"); // no css functionality, just for easier use later
+  }
+  else if (justActivate && justDeactivate) {
+    return;
+  }
+  else if (justActivate) { 
+    dropdownMenu.classList.add("active");
+    dropdownDiv.classList.add("active");
+  }
+  else if (justDeactivate) {
+    dropdownMenu.classList.remove("active");
+    dropdownDiv.classList.remove("active");
+  }
+}
+
+// closes all dropdowns. If except specifies a header div, skip that one
+function closeAllDropdown(except=undefined) {
+  headerDiv.forEach((div) => {
+    if (except != undefined && div == except) {
+      return;
+    }
+    div.classList.remove("active");
+    let dropdownMenu = div.querySelector(".dropdown-content");
+    dropdownMenu.classList.remove("active");
+  });
+}
+    
 let headerButtons = document.querySelectorAll(".dropdown-btn");
+let headerDiv = document.querySelectorAll(".dropdown");
+
+// event listener for button clicks
 headerButtons.forEach((btn) => { btn.addEventListener('click', (e) => {
-  let dropdownMenu = e.currentTarget.parentNode.querySelector(".dropdown-content");
-  dropdownMenu.classList.toggle("active");
+  closeAllDropdown(e.currentTarget.parentNode); // call this first to close all other dropdowns that may be open
+  toggleDropdown(e.currentTarget.parentNode);
   });
 });
+
+// for clicks outside the dropdown, which close the menu if it was open before
+window.onclick =  (e) => {
+  let classes = e.target.parentNode.classList;
+  if (classes != undefined && classes.length > 0 && classes[0] === "dropdown") {
+    return;
+  }
+  closeAllDropdown();
+};
+
+/* for hover. temporarily disabled since using both clicking and hover is counterintuitive (?)
+
+// event listener for dropdown hover - only opens, and keeps open since it targets entire div
+headerDiv.forEach((div) => {div.addEventListener('mouseenter', (e) => {
+  console.log(e);
+  toggleDropdown(e.currentTarget, true);
+});
+});
+
+// event listener for closing the dropdown after hover is over
+headerDiv.forEach((div) => {div.addEventListener('mouseout', (e) => {
+  toggleDropdown(e.currentTarget, undefined, true);
+});
+});
+*/
