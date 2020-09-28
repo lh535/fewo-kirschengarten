177f0877d7e35d3cb547fc4981372292d14c6af5
lh535 <hofertlisa@gmail.com>
2020-09-28 23:03:17 +0200

    added Anfahrt page

--- anfahrt.php
+++ anfahrt.php
@@ -17,12 +17,24 @@
       <h1>Anfahrt</h1>
       <div class='content-wrapper' id='description-content'>
         <div class='paragraph'>
-          <p class='main-text'>
-            Benutz doch einfach ein Navi.
+          <div>
+          <p>
+            <b>Adresse:</b><br>
+            Ferienwohnung Kirschengarten<br>
+            Sasbacherstr. 4<br>
+            79346 Endingen-Königschaffhausen<br><br>
           </p>
+          <p>
+          Ihr Auto können Sie gerne unter der Überdachung direkt vor der Wohnung abstellen. Melden Sie sich bei der Ankunft einfach gleich bei unserem Haus, welches sich links von der Wohnung befindet.  
+          </p>
+          </div>
+          <img src="images/air_photo.jpg" alt="Luftbild des Hauses">
+        </div>
+        <div class='paragraph'>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5325.629102017168!2d7.650359307991996!3d48.13309961923682!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x479115901af4558d%3A0x8e83736ecaf92e3!2sSasbacher%20Str.%204%2C%2079346%20Endingen%20am%20Kaiserstuhl!5e0!3m2!1sen!2sde!4v1601321773721!5m2!1sen!2sde" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
         </div>
       </div>
-    <?php include 'footer.php'; ?>
+      <?php include 'footer.php'; ?>
     </div>
   </body>
   <script type='text/javascript' src='scripts/navbar.js'></script>
