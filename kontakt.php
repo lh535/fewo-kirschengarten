322eef82e431cfa93060f0b31257880019a3652f
lh535 <hofertlisa@gmail.com>
2020-09-30 01:35:55 +0200

    completed contact page closes #26

--- kontakt.php
+++ kontakt.php
@@ -18,10 +18,21 @@
       <h1>Kontakt</h1>
       <div class='content-wrapper' id='description-content'>
         <div class='paragraph'>
+          <div>
           <p>
-            Sie wollen Buchen oder haben eine Frage zur Wohnung? Kontaktieren Sie uns! 
+            <b>Kontaktmöglichkeiten</b><br>
+            Telefon: 07642/1290<br>
+            Email: <a href=info@fewo-kirschengarten.de>info@fewo-kirschengarten.de</a><br>
           </p>
-          <img ref=''>
+          <p>
+            <b>Anschrift</b><br>
+            Ferienwohnung Kirschengarten<br>
+            Klaus und Petra Hofert<br>
+            Sasbacher Str. 4<br>
+            79346 Endingen-Königschaffhausen<br>
+          </p>
+          </div>
+          <img src="images/home_background.jpg" alt="Aquarell-Logo der Ferienwohnung">
         </div>
       </div>
     <?php include 'footer.php'; ?>
