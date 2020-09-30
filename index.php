681dbe500a8658a403aa45db1c0d13caa4a3c76c
lh535 <hofertlisa@gmail.com>
2020-09-30 22:31:39 +0200

    added buttons, fadein to homepage. closes #4

--- index.php
+++ index.php
@@ -15,8 +15,13 @@
     <div id='homepage-main'>
     <script>setTopOffset("#homepage-main")</script>
       <div id='homepage-logo'>
-        <p id='home_small_logo'>FERIENWOHNUNG</p>
-        <p id='home_big_logo'>KIRSCHENGARTEN</p>
+        <p id='home_small_logo'>Ferienwohnung</p>
+        <p id='home_big_logo'>Kirschengarten</p>
+      </div>
+      <div id='home_button_div'>
+        <button type=button onclick="location.href='beschreibung.php'" class='home_buttons' id='home_button_1'><b>BESCHREIBUNG</b></button>
+        <button type=button onclick="location.href='preisliste.php'" class='home_buttons' id='home_button_2'><b>PREISE</b></button>
+        <button type=button onclick="location.href='belegungsplan.php'" class='home_buttons' id='home_button_3'><b>BELEGUNGSPLAN</b></button>
       </div>
     <?php include 'footer.php'; ?>
     </div> 
