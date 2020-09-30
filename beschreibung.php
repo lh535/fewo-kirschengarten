c2b9ef87b84d287bdd48682ccc138dd90cda2824
lh535 <hofertlisa@gmail.com>
2020-09-30 23:44:24 +0200

    added favicon, put head info into separate file that is now linked to

--- beschreibung.php
+++ beschreibung.php
@@ -1,14 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <meta charset='UTF-8'>
-    <title>Ferienwohnung Kirschengarten</title>
-    <link rel='stylesheet' type='text/css' href='styles/normalize.css'>
-    <link rel='stylesheet' type='text/css' href='styles/custom-reset.css'>
-    <link rel='stylesheet' type='text/css' href='styles/main.css'>
-    <script type='text/javascript' src='scripts/main.js'></script>
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Roboto&family=Roboto+Condensed:wght@400;700&display=swap" rel="stylesheet"> 
-
+    <?php include 'head.php'; ?>
   </head>
   <body>
     <?php include 'header.php'; ?>
