943f38b51ca7b2c2135b92cb3a6f3dc3417c976a
lh535 <hofertlisa@gmail.com>
2020-10-31 00:48:17 +0100

    add meta data. closes #16

--- head.php
+++ head.php
@@ -6,3 +6,13 @@
 <script type='text/javascript' src='scripts/main.js'></script>
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Roboto&family=Roboto+Condensed:wght@400;700&display=swap" rel="stylesheet"> 
 <link rel="icon" href="images/favicon.ico">
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="description" content="Fünf-Sterne Ferienwohnung am Kaiserstuhl"
+<meta name="author" content="Lisa Hofert">
+<!--[if It IE 9]>
+  <script>
+  alert("Sie nutzen einen veralteten Browser. Die Seite funktioniert möglicherweise nicht vollständig. Bitte upgraden Sie ihren Browser, oder melden Sie sich bei Problemen hier: hofertlisa@gmail.com");
+</script>
+<![endif]-->
