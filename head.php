e41a2efc830c380fea9b67f8da470732dc05bd9b
Lisa Hofert <hofertlisa@gmail.com>
2022-06-08 18:41:58 +0200

    smaller file sizes, cumulative css file added

--- head.php
+++ head.php
@@ -1,8 +1,6 @@
 <meta charset='UTF-8'>
 <title>Ferienwohnung Kirschengarten</title>
-<link rel='stylesheet' type='text/css' href='styles/normalize.css'>
-<link rel='stylesheet' type='text/css' href='styles/custom-reset.css'>
-<link rel='stylesheet' type='text/css' href='styles/main.css'>
+<link rel='stylesheet' type='text/css' href='styles/total.css'>
 <script type='text/javascript' src='scripts/main.js'></script>
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Roboto&family=Roboto+Condensed:wght@400;700&display=swap" rel="stylesheet"> 
 <!-- <link href="https://fonts.googleapis.com/css2?family=Great+Vibes&display=swap" rel="stylesheet"> -->
