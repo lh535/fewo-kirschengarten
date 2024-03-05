7808f4ef5603da85935d182a1b12c704bb0e8876
Lisa Hofert <hofertlisa@gmail.com>
2024-03-05 07:38:03 +0100

    fix broken link

--- belegungsplan.php
+++ belegungsplan.php
@@ -11,7 +11,7 @@
       <h1>Belegungsplan</h1>
       <div class='content-wrapper' id='description-content'>
         <div class='paragraph'>
-          <iframe src='https://tportal.toubiz.de/kaiserstuhl-gastgeber/ukv/calendar/FIT00020070312698240'></iframe>
+          <iframe src='https://tportal.tomas.travel/kaiserstuhl-gastgeber/ukv/calendar/FIT00020070312698240'></iframe>
         </div>
         <div style="text-align: center;">
         <p> Anreise: ab 15:00 Uhr &nbsp; &nbsp; Abreise: bis 10:00 Uhr <br>
